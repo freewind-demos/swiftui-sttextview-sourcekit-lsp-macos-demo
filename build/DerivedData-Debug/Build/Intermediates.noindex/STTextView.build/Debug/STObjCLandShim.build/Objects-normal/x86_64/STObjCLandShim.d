708497b32e863332006bac700c4e288470a2211e
Freewind <nowindlee@gmail.com>
2026-07-29 22:16:52 +0800

    Quick Commit

---
+++ build/DerivedData-Debug/Build/Intermediates.noindex/STTextView.build/Debug/STObjCLandShim.build/Objects-normal/x86_64/STObjCLandShim.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/peng.li/workspace/freewind-demos/swiftui-sttextview-sourcekit-lsp-macos-demo/build/DerivedData-Debug/SourcePackages/checkouts/STTextView/Sources/STObjCLandShim/include/module.modulemap \
+  /Users/peng.li/workspace/freewind-demos/swiftui-sttextview-sourcekit-lsp-macos-demo/build/DerivedData-Debug/SourcePackages/checkouts/STTextView/Sources/STObjCLandShim/STObjCLandShim.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/TargetConditionals.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Cocoa.framework/Modules/module.modulemap \
+  /Users/peng.li/workspace/freewind-demos/swiftui-sttextview-sourcekit-lsp-macos-demo/build/DerivedData-Debug/SourcePackages/checkouts/STTextView/Sources/STObjCLandShim/include/CGContextShim.h
